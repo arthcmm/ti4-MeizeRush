3448070e6d8d23b6b00b727cc79b9508d9662226
Leonardo <leonardo@Leonardos-MacBook-Pro.local>
2024-04-17 20:57:16 -0300

    added craft menu

---
+++ MeizeRush/Assets/Scripts/CraftMenuScript.cs
@@ -0,0 +1,61 @@
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class CraftMenuScript : MonoBehaviour
+{
+    public GameObject buttonPrefab; // Assign in inspector
+    public RectTransform buttonsContainer; // Assign the Panel or a specific container within the Panel
+
+    private List<string> itemList = new List<string>() { "Item 1", "Item 2", "Item 3" }; // Example list
+    private Button selectedButton = null; // To keep track of the selected button
+    private Color selectedColor = Color.green; // Color for selected button
+    private Color normalColor = Color.white; // Normal color for buttons
+
+    void Start()
+    {
+        GenerateButtons();
+    }
+
+    void GenerateButtons()
+    {
+        foreach (string item in itemList)
+        {
+            GameObject buttonGO = Instantiate(buttonPrefab, buttonsContainer);
+            buttonGO.GetComponentInChildren<TMP_Text>().text = item;
+            Button button = buttonGO.GetComponent<Button>();
+            button.onClick.AddListener(() => SelectItem(item, button));
+
+            // Set the initial color of the button
+            ColorBlock colors = button.colors;
+            colors.normalColor = normalColor;
+            button.colors = colors;
+        }
+    }
+
+    void SelectItem(string itemName, Button button)
+    {
+        if (selectedButton != null)
+        {
+            // Reset the previous selected button color
+            ColorBlock prevColors = selectedButton.colors;
+            prevColors.normalColor = normalColor;
+            selectedButton.colors = prevColors;
+        }
+
+        // Set the new selected button and change its color
+        selectedButton = button;
+        ColorBlock newColors = selectedButton.colors;
+        newColors.selectedColor = selectedColor;
+        selectedButton.colors = newColors;
+    }
+
+    public void LogSelectedItem()
+    {
+        if (selectedButton != null)
+            Debug.Log("Crafted: " + selectedButton.GetComponentInChildren<TMP_Text>().text);
+        else
+            Debug.Log("No item selected.");
+    }
+}
---
+++ MeizeRush/Assets/Scripts/CrafttableScript.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrafttableScript : MonoBehaviour {
+  public GameObject player;
+  public GameObject menu;
+
+  private bool isOnRange;
+
+  // Start is called before the first frame update
+  private void OnTriggerEnter(Collider other) {
+    if (other.gameObject == player) {
+      // Player has entered the interaction zone
+      Debug.Log("Player in range");
+      isOnRange = true;
+    }
+  }
+
+  private void OnTriggerExit(Collider other) {
+    if (other.gameObject == player) {
+      // Player has left the interaction zone
+      Debug.Log("Player out of range");
+      isOnRange = false;
+    }
+  }
+
+  private void Update()
+{
+    if (isOnRange)
+    {
+      if(Input.GetKeyDown(KeyCode.E)){
+        OpenMenu();
+      }
+    }else{
+      CloseMenu();
+    }
+}
+void OpenMenu()
+  {
+    menu.SetActive(true);
+  }
+  void CloseMenu() {
+    menu.SetActive(false);
+  }
+}
