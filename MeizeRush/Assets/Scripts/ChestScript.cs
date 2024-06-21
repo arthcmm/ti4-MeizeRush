e7918f5f58cf646c3221e6c215b42f64c55793fc
leobuldrini <leo.buldrini@hotmail.com>
2024-06-21 10:35:02 -0300

    redone chest chance

--- MeizeRush/Assets/Scripts/ChestScript.cs
+++ MeizeRush/Assets/Scripts/ChestScript.cs
@@ -2,16 +2,17 @@ using System.Collections;
 using UnityEngine;
 using TMPro;
 
-public class ChestScript : MonoBehaviour
-{
+public class ChestScript : MonoBehaviour {
   private Transform player;
-  [SerializeField] int gemScore = 100;
-  [SerializeField] int scrapFound = 30;
+  [SerializeField]
+  int gemScore = 100;
+  [SerializeField]
+  int scrapFound = 30;
   public AudioSource audioSource;
   public AudioClip gemAudio, dmgAudio, lifeAudio, matAudio;
   private GameControllerScript gc;
   public PlayerScript ps;
-  public float distancia; //1.2 parece um bom valor
+  public float distancia; // 1.2 parece um bom valor
   private bool aberto;
   public Sprite damageChest;
   public Sprite diamondChest;
@@ -23,8 +24,7 @@ public class ChestScript : MonoBehaviour
   public GameObject floatingTextPrefab;
   private Canvas mainCanvas; // Adicione uma referência ao Canvas principal
 
-  void Awake()
-  {
+  void Awake() {
     audioSource = GameObject.FindGameObjectWithTag("ChestSound")
                       .GetComponent<AudioSource>();
     mainCanvas =
@@ -34,8 +34,7 @@ public class ChestScript : MonoBehaviour
         GameObject.FindGameObjectWithTag("NavMesh").GetComponent<PetNavMesh>();
   }
 
-  void Start()
-  {
+  void Start() {
     aberto = false;
     player = GameObject.FindGameObjectWithTag("Player").transform;
     ps =
@@ -43,103 +42,81 @@ public class ChestScript : MonoBehaviour
     gc = GameObject.FindGameObjectWithTag("Controller")
              .GetComponent<GameControllerScript>();
   }
-  void Update()
-  {
-    if (Vector3.Distance(transform.position, player.position) <= distancia)
-    {
-      if (Input.GetKeyDown(KeyCode.E))
-      {
-        if (!aberto)
-        {
+  void Update() {
+    if (Vector3.Distance(transform.position, player.position) <= distancia) {
+      if (Input.GetKeyDown(KeyCode.E)) {
+        if (!aberto) {
           aberto = true;
-          int item = Random.Range(0, 9);
-          switch (item)
-          {
-            case 0:
-            case 1:
-            case 2:
-            case 3:
-              Debug.Log("Você ganhou uma GEMA!");
-              audioSource.clip = gemAudio;
-              gc.score += gemScore;
-              gameObject.GetComponent<SpriteRenderer>().sprite = diamondChest;
-              break;
-            case 4:
-              gameObject.GetComponent<SpriteRenderer>().sprite = heartChest;
-              Debug.Log("Você recuperou VIDA!");
-              audioSource.clip = lifeAudio;
-              if (ps.life + 10 >= 100)
-              {
-                ps.life = 100;
-              }
-              else if (ps.life < 100)
-              {
-                ps.life += 10;
-              }
-              break;
-            case 5:
-              Debug.Log("Você ganhou um UPGRADE DE DANO!");
-              audioSource.clip = dmgAudio;
-              ps.attackDamage += 5;
-              gameObject.GetComponent<SpriteRenderer>().sprite = damageChest;
-              break;
-            case 6:
-            case 7:
-            case 8:
-              Debug.Log("Você ganhou MATERIAIS!");
-              audioSource.clip = matAudio;
-              gc.scrap += scrapFound;
-              gameObject.GetComponent<SpriteRenderer>().sprite = gearChest;
-              break;
-            case 9:
-              Debug.Log("Você ganhou um PET NOVO!");
-              gameObject.GetComponent<SpriteRenderer>().sprite = petChest;
-              pnm.comecar();
-              break;
-            default:
-              break;
+          int item = Random.Range(0, 10);
+          switch (item) {
+          case 0:
+          case 1:
+          case 2:
+          case 3:
+            Debug.Log("Você ganhou uma GEMA!");
+            audioSource.clip = gemAudio;
+            gc.score += gemScore;
+            gameObject.GetComponent<SpriteRenderer>().sprite = diamondChest;
+            break;
+          case 4:
+            gameObject.GetComponent<SpriteRenderer>().sprite = heartChest;
+            Debug.Log("Você recuperou VIDA!");
+            audioSource.clip = lifeAudio;
+            if (ps.life + 10 >= 100) {
+              ps.life = 100;
+            } else if (ps.life < 100) {
+              ps.life += 10;
+            }
+            break;
+          case 5:
+            Debug.Log("Você ganhou um UPGRADE DE DANO!");
+            audioSource.clip = dmgAudio;
+            ps.attackDamage += 5;
+            gameObject.GetComponent<SpriteRenderer>().sprite = damageChest;
+            break;
+          case 6:
+          case 7:
+          case 8:
+            Debug.Log("Você ganhou MATERIAIS!");
+            audioSource.clip = matAudio;
+            gc.scrap += scrapFound;
+            gameObject.GetComponent<SpriteRenderer>().sprite = gearChest;
+            break;
+          case 9:
+            Debug.Log("Você ganhou um PET NOVO!");
+            gameObject.GetComponent<SpriteRenderer>().sprite = petChest;
+            pnm.comecar();
+            break;
+          default:
+            break;
           }
           audioSource.Play();
           StartCoroutine(DeactivateAfterAudio());
-        }
-        else Debug.Log("Baú já aberto");
+        } else
+          Debug.Log("Baú já aberto");
       }
     }
   }
 
-
-
-
-
-  private void ShowFloatingText(string message)
-  {
-    if (floatingTextPrefab)
-    {
+  private void ShowFloatingText(string message) {
+    if (floatingTextPrefab) {
       GameObject floatingText = Instantiate(
           floatingTextPrefab, transform.position, Quaternion.identity);
       TextMeshPro textMesh = floatingText.GetComponentInChildren<TextMeshPro>();
-      if (textMesh != null)
-      {
+      if (textMesh != null) {
         textMesh.text = message;
-      }
-      else
-      {
+      } else {
         Debug.LogError(
             "TextMeshPro component not found on the floating text prefab!");
       }
-    }
-    else
-    {
+    } else {
       Debug.LogError("FloatingTextPrefab is not assigned in the inspector!");
     }
   }
 
-  private IEnumerator DeactivateAfterAudio()
-  {
+  private IEnumerator DeactivateAfterAudio() {
     yield return new WaitWhile(() => audioSource.isPlaying);
     yield return new WaitForSeconds(2f);
     gameObject.SetActive(false);
   }
 }
-
-
