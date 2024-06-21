1c03aaff7e64cea4476d86b41d2d2e3fb1b4cb68
leobuldrini <leo.buldrini@hotmail.com>
2024-06-21 10:41:25 -0300

    aha

--- MeizeRush/Assets/Scripts/CrafttableScript.cs
+++ MeizeRush/Assets/Scripts/CrafttableScript.cs
@@ -2,105 +2,83 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CrafttableScript : MonoBehaviour
-{
-    public GameObject menuPrefab; // Assign this in the Unity Inspector
-    private GameObject menuInstance;
-    private GameObject player;
-    public GameObject mesa;
-    private bool isOnRange;
-    public GameControllerScript gameControllerScript;
-    public PlayerScript playerScript;
+public class CrafttableScript : MonoBehaviour {
+  public GameObject menuPrefab; // Assign this in the Unity Inspector
+  private GameObject menuInstance;
+  private GameObject player;
+  public GameObject mesa;
+  private bool isOnRange;
+  public GameControllerScript gameControllerScript;
+  public PlayerScript playerScript;
 
+  void Start() {
+    player = GameObject.FindGameObjectWithTag("Player");
+    playerScript = player.GetComponent<PlayerScript>();
+    gameControllerScript = GameObject.FindGameObjectWithTag("Controller")
+                               .GetComponent<GameControllerScript>();
+  }
 
-    void Start()
-    {
-        player = GameObject.FindGameObjectWithTag("Player");
-        playerScript = player.GetComponent<PlayerScript>();
-        gameControllerScript = GameObject.FindGameObjectWithTag("Controller").GetComponent<GameControllerScript>();
+  private void OnTriggerEnter2D(Collider2D other) {
+    if (other.gameObject == player) {
+      isOnRange = true;
     }
+  }
 
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.gameObject == player)
-        {
-            isOnRange = true;
-        }
+  private void OnTriggerExit2D(Collider2D other) {
+    if (other.gameObject == player) {
+      isOnRange = false;
+      CloseMenu();
     }
+  }
 
-    private void OnTriggerExit2D(Collider2D other)
-    {
-        if (other.gameObject == player)
-        {
-            isOnRange = false;
-            CloseMenu();
-        }
+  private void Update() {
+    if (isOnRange && Input.GetKeyDown(KeyCode.E) &&
+        gameControllerScript.scrap > 0) {
+      // OpenMenu();
+      int times = gameControllerScript.scrap / 15;
+      for (int i = 0; i < times; i++) {
+        giveItem("Trap");
+      }
+      mesa.SetActive(false);
     }
+  }
 
-    private void Update()
-    {
-        if (isOnRange && Input.GetKeyDown(KeyCode.E) && gameControllerScript.scrap > 0)
-        {
-            //OpenMenu();
-            for (int i = 0; i < gameControllerScript.scrap / 15; i++)
-            {
-                giveItem("Trap");
-            }
-            mesa.SetActive(false);
-        }
+  void OpenMenu() {
+    if (menuInstance == null) { // Ensure you don't create multiple menus
+      menuInstance = Instantiate(menuPrefab);
+    } else {
+      menuInstance.SetActive(true);
     }
+  }
 
-    void OpenMenu()
-    {
-        if (menuInstance == null)
-        { // Ensure you don't create multiple menus
-            menuInstance = Instantiate(menuPrefab);
-        }
-        else
-        {
-            menuInstance.SetActive(true);
-        }
+  void CloseMenu() {
+    if (menuInstance != null) {
+      Destroy(menuInstance);
+      menuInstance = null; // Ensure the reference is cleared
     }
+  }
 
-    void CloseMenu()
-    {
-        if (menuInstance != null)
-        {
-            Destroy(menuInstance);
-            menuInstance = null; // Ensure the reference is cleared
-        }
-    }
-
-    private int canCraft(string itemName)
-    {
-        Dictionary<string, int> resources = new Dictionary<string, int> {
+  private int canCraft(string itemName) {
+    Dictionary<string, int> resources = new Dictionary<string, int> {
       { "Trap", 15 }, { "Item 2", 3 }, { "Item 3", 4 }
     };
-        int cost = resources[itemName];
-        if (gameControllerScript.scrap >= cost)
-        {
-            gameControllerScript.scrap -= cost;
-            return gameControllerScript.scrap;
-        }
-        else
-        {
-            return -1;
-        }
+    int cost = resources[itemName];
+    if (gameControllerScript.scrap >= cost) {
+      gameControllerScript.scrap -= cost;
+      return gameControllerScript.scrap;
+    } else {
+      return -1;
     }
+  }
 
-    public void giveItem(string itemName)
-    {
-        if (canCraft(itemName) >= 0)
-        {
-            if (itemName == "Trap")
-            {
-                playerScript.availableTraps++;
-            }
-            Debug.Log("Crafted: " + itemName);
-        }
-        else
-        {
-            Debug.Log("Not enough resources to craft " + itemName);
-        }
+  public void giveItem(string itemName) {
+    if (canCraft(itemName) >= 0) {
+      if (itemName == "Trap") {
+        playerScript.availableTraps++;
+      }
+      Debug.Log("Crafted: " + itemName);
+    } else {
+      Debug.Log("Not enough resources to craft " + itemName);
     }
+  }
 }
